9f603992709a9b79f759b046e29f69424dab0489
BonDoQ <bondoq@eventtus.com>
2014-05-30 21:06:55 +0300

    Add Checker Class

---
+++ Checker.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * File for Sudoku Checker Class
+ *
+ * PHP version 5.5
+ *
+ * @category Programming_Technique
+ * @package  Sudoku
+ * @author   bondoq <bondoq@eventtus.com>
+ * @license  Apache <http://www.apache.org/licenses/LICENSE-2.0.html>
+ * @link     <http://github/bondoq>
+ */
+namespace Sudoku;
+
+/**
+ * Class Checker
+ * used to Check Sudoku Puzzle Solution
+ *
+ * @category Programming_Technique
+ * @package  Sudoku
+ * @author   bondoq <bondoq@eventtus.com>
+ * @license  Apache <http://www.apache.org/licenses/LICENSE-2.0.html>
+ * @link     <http://github/bondoq>
+ */
+class Checker
+{
+
+    /**
+     * @var array() Puzzle to Check Solution
+     */
+    protected $puzzle;
+
+
+    /**
+     * Checker Constructor
+     *
+     * @param array() $puzzle Puzzle to Check Solution
+     */
+    public function __construct($puzzle)
+    {
+        $this->puzzle = $puzzle;
+
+    }
+
+
+    /**
+     * extract sub of a Puzzle into linear array
+     *
+     * @param Int $xStart X-axis Start Position
+     * @param Int $xEnd   X-axis End Position
+     * @param Int $yStart Y-axis Start Position
+     * @param Int $yEnd   Y-axis End Position
+     *
+     * @return array() the linear array
+     */
+    public function subArray($xStart, $xEnd, $yStart, $yEnd)
+    {
+        $linearArray = [];
+        for ($y = $yStart; $y <= $yEnd; $y++) {
+            for ($x = $xStart; $x <= $xEnd; $x++) {
+                $linearArray[] = $this->puzzle[$y][$x];
+            }
+        }
+
+        return $linearArray;
+
+    }
+
+    /**
+     * Convert Sudoku Matrix to array of linear arrays to check duplicates
+     *
+     * @return array LinearArrays to check for Puzzle
+     */
+    public function prepareLinearArrays()
+    {
+        $linear = [];
+        for ($r = 0; $r < 9; $r++) {
+            // Vertical Linear.
+            $linear[] = $this->subArray($r, $r, 0, 8);
+
+            // Horizontal Linear.
+            $linear[] = $this->subArray(0, 8, $r, $r);
+
+            if (($r % 3) === 0) {
+                // Upper Small Square.
+                $linear[] = $this->subArray($r, ($r + 2), 0, 2);
+
+                // Middle Small Square.
+                $linear[] = $this->subArray($r, ($r + 2), 3, 5);
+
+                // Lower Small Square.
+                $linear[] = $this->subArray($r, ($r + 2), 6, 8);
+            }
+        }
+
+        return $linear;
+
+    }
+
+
+    /**
+     * Check Sudoku Puzzle Solution
+     *
+     * @return bool
+     */
+    public function check()
+    {
+        $linear = $this->prepareLinearArrays();
+        foreach ($linear as $array) {
+            if (count(array_unique($array)) < count($array)) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+
+}
+
+?>
---
+++ CheckerTest.php
@@ -0,0 +1,98 @@
+<?php
+
+include 'Checker.php';
+
+class CheckerTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Checker Instant of Checker Class
+     */
+    protected $checker;
+
+    /**
+     * @var array() Puzzle TestCase
+     */
+    protected $puzzleOne;
+
+    /**
+     * @var array() Another Puzzle TestCase
+     */
+    protected $puzzleTwo;
+
+
+    /**
+     * Setup Checker Class with two Puzzles as TestCase
+     */
+    public function setUp()
+    {
+        $this->puzzleOne = [
+            [8, 5, 7, 4, 1, 9, 2, 3, 6],
+            [3, 6, 2, 8, 7, 5, 4, 1, 9],
+            [4, 9, 1, 2, 3, 6, 7, 5, 8],
+            [1, 3, 9, 7, 2, 4, 6, 8, 5],
+            [7, 4, 6, 1, 5, 8, 9, 2, 3],
+            [2, 8, 5, 6, 9, 3, 1, 7, 4],
+            [6, 7, 4, 5, 8, 1, 3, 9, 2],
+            [5, 1, 3, 9, 6, 2, 8, 4, 7],
+            [9, 2, 8, 3, 4, 7, 5, 6, 1],
+        ];
+
+        $this->puzzleTwo = [
+            [5, 7, 4, 2, 9, 6, 1, 3, 8],
+            [3, 8, 6, 7, 1, 5, 2, 4, 9],
+            [9, 2, 1, 3, 4, 8, 6, 5, 7],
+            [2, 4, 5, 1, 8, 3, 9, 7, 6],
+            [7, 9, 3, 6, 5, 2, 8, 1, 4],
+            [6, 1, 8, 9, 7, 4, 5, 2, 3],
+            [4, 3, 9, 8, 2, 1, 7, 6, 5],
+            [8, 6, 2, 5, 3, 7, 4, 9, 1],
+            [1, 5, 7, 4, 6, 9, 3, 8, 2],
+        ];
+        $this->checker = new Checker($this->puzzleOne);
+    }
+
+    /**
+     * Unit test for SubArray Function in Checker Class
+     */
+    public function testCheckerSubArray()
+    {
+        //check Horizontal Sub-Array
+        $linear = $this->checker->subArray(0, 8, 0, 0);
+        $this->assertInternalType('array', $linear);
+        $this->assertEquals([8, 5, 7, 4, 1, 9, 2, 3, 6], $linear);
+
+        //Check Grid Sub-Array
+        $linear = $this->checker->subArray(0, 2, 0, 2);
+        $this->assertInternalType('array', $linear);
+        $this->assertEquals([8, 5, 7, 3, 6, 2, 4, 9, 1], $linear);
+
+        //Check Vertical Sub-Array
+        $linear = $this->checker->subArray(0, 0, 0, 8);
+        $this->assertInternalType('array', $linear);
+        $this->assertEquals([8, 3, 4, 1, 7, 2, 6, 5, 9], $linear);
+
+    }
+
+    /**
+     * Unit Test for PrepareLinearArrays function in Checker Class
+     */
+    public function testPrepareLinearArrays()
+    {
+        $arrays = $this->checker->prepareLinearArrays();
+        $this->assertInternalType('array', $arrays);
+        $this->assertEquals(27, count($arrays));
+    }
+
+    /**
+     * Unit Test for Check function in Checker Class
+     */
+    public function testCheckerClass()
+    {
+        $result = $this->checker->check($this->puzzleOne);
+        $this->assertEquals(true, $result);
+    }
+
+}
+
+?>
